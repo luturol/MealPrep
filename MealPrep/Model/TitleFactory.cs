19bd24793291479642f1a59de353855dbd79dbcc
Rafael Ahrons <rafael.ahrons@gmail.com>
2018-07-24 21:48:55 -0300

    Adding TitleFactory and FrmAuxiliary

---
+++ MealPrep/Model/TitleFactory.cs
@@ -0,0 +1,27 @@
+﻿using MealPrep.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealPrep.Model
+{
+    public class TitleFactory
+    {
+        private static readonly String TITLE_NEW_FOOD = "New Food";
+        private static readonly String ERROR_TITLE_NOT_FOUND = "Title not found";
+
+        public static String GetTitle(Type type)
+        {
+            if (type.Equals(typeof(ucAddFood)))
+            {
+                return TITLE_NEW_FOOD;
+            }
+            else
+            {
+                throw new Exception(ERROR_TITLE_NOT_FOUND);
+            }
+        }
+    }
+}
---
+++ MealPrep/View/FrmAuxiliary.cs
@@ -0,0 +1,27 @@
+﻿using MealPrep.Useful;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MealPrep.View
+{
+    public partial class frmAuxiliary : Form
+    {
+        private UserControl userControl;
+
+        public frmAuxiliary(UserControl userControl)
+        {
+            this.userControl = userControl;
+            InitializeComponent();
+            this.Controls.Add(userControl);
+            UsefulAlgorithms.AdjustFormSize(this);
+            this.Text = "";
+        }
+    }
+}
